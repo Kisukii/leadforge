23b1981a5262254a2fae5cbc43077459c588a05b
Nandana Sunil <nandanasunil2032006@gmail.com>
2026-05-24 12:23:58 +0530

    fixed port for deployment

--- server/Server/index.js
+++ server/Server/index.js
@@ -73,6 +73,8 @@ app.get("/analytics", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
